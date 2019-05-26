c76936bde03752a92025abdaadefa5ffba067c67
Ethan Nelson <ethan.charles@gmail.com>
2019-05-26 15:06:44 -0700

    cleanup

--- LanguagePatternsAndExtensions/Option.cs
+++ LanguagePatternsAndExtensions/Option.cs
@@ -40,22 +40,4 @@ namespace LanguagePatternsAndExtensions
             return new Option<T>(Enumerable.Empty<T>());
         }
     }
-
-    public static class OptionExtensions
-    {
-        public static Option<T> Some<T>(this T item)
-        {
-            return Option<T>.Some(item.AsEnumerable());
-        }
-
-        public static Option<T> None<T>()
-        {
-            return Option<T>.None();
-        }
-
-        public static Option<T> ToOption<T>(this IEnumerable<T> item)
-        {
-            return new Option<T>(item);
-        }
-    }
 }
---
+++ LanguagePatternsAndExtensions/OptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LanguagePatternsAndExtensions
+{
+    public static class OptionExtensions
+    {
+        public static Option<T> Some<T>(this T item)
+        {
+            return Option<T>.Some(item.AsEnumerable());
+        }
+
+        public static Option<T> None<T>()
+        {
+            return Option<T>.None();
+        }
+
+        public static Option<T> ToOption<T>(this IEnumerable<T> item)
+        {
+            return new Option<T>(item);
+        }
+    }
+}
